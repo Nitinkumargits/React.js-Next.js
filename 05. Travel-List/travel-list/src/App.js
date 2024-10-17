1946ce7a4eb5c70cd6468124fa45ba2e5af3ee37
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-10-18 01:02:05 +0530

    Rendering item

--- 05. Travel-List/travel-list/src/App.js
+++ 05. Travel-List/travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 2, packed: true },
+];
 function App() {
   return (
     <div className="app">
@@ -20,7 +25,26 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  const linethr = { textDecoration: "line-through" };
+  return (
+    <li>
+      <span style={item.packed ? linethr : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
