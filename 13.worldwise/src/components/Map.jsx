4af89208514c0bfc8f52536bf51a673dcb6fd923
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-11-15 00:26:27 +0530

    useSearchParams Hooks

--- 13.worldwise/src/components/Map.jsx
+++ 13.worldwise/src/components/Map.jsx
@@ -1,7 +1,18 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Postion: {lat}, {lng}
+      </h1>
+    </div>
+  );
 }
 
 export default Map;
