154ad697bae142f98fefecbcbfbb0100f806b5aa
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-12-08 14:25:36 +0530

    tailwind : style with React Component

--- 17.fast-react-pizza/src/features/user/Username.jsx
+++ 17.fast-react-pizza/src/features/user/Username.jsx
@@ -1,7 +1,5 @@
 function Username() {
-  return (
-    <div className="hidden text-sm font-semibold md:block">Nitin kumar </div>
-  );
+  return <div className="hidden text-sm font-semibold md:block"></div>;
 }
 
 export default Username;
---
+++ 17.fast-react-pizza/src/ui/Button.jsx
@@ -0,0 +1,29 @@
+/* eslint react/prop-types: 0 */
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to }) {
+  const className =
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
+  // if (onClick)
+  // return (
+  //   <button onClick={onClick} disabled={disabled} className={className}>
+  //     {children}
+  //   </button>
+  // );
+
+  return (
+    <button disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
