76e41b313f72076ef80281855cf7de79934d2f88
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-12-09 14:49:24 +0530

    menuItem to cart

--- 17.fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 17.fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,10 +1,29 @@
 /* eslint-disable react/prop-types */
-
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
+// import DeleteItem from "../cart/DeleteItem";
+// import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 import { formatCurrency } from "../../utils/helpers";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
-  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -26,7 +45,12 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+
+          {!soldOut && !isInCart && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
