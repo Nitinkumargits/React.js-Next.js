6b158afff08417ab54679fce67479296a7ecdd34
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-12-10 15:21:28 +0530

    Resuable styled Component

--- 18.the-wild-oasis/src/App.jsx
+++ 18.the-wild-oasis/src/App.jsx
@@ -2,14 +2,11 @@ import styled from "styled-components";
 import GloablStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orange;
+  /* background-color: orange; */
   padding: 20px;
 `;
 
@@ -18,10 +15,33 @@ function App() {
     <>
       <GloablStyles />
       <StyledApp>
-        <H1>Hellow world</H1>
-        <Button onClick={() => alert("alert")}>Check in.</Button>
-        <Button onClick={() => alert("alert")}>Check out</Button>
-        <Input type="number" placeholder="No. of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hellow world</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert("alert")}>
+                Check in.
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("alert")}>
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="No. of guests" />
+              <Input type="number" placeholder="No. of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
