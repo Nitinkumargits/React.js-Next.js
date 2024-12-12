882071ccbe3b16eabcc43a9a107ac40e2a7288ec
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-12-12 10:35:05 +0530

    implement form

--- 18.the-wild-oasis/src/pages/Cabins.jsx
+++ 18.the-wild-oasis/src/pages/Cabins.jsx
@@ -1,7 +1,12 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
 import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -10,6 +15,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
