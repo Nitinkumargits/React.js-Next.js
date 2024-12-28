3128715fc5161ef4cfc498de1f0a6a20616d3177
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-12-28 12:45:32 +0530

    set up auth

--- 20.the-wild-oasis-website/app/cabins/page.js
+++ 20.the-wild-oasis-website/app/cabins/page.js
@@ -1,12 +1,19 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
+
+export const revalidate = 3600;
+// export const revalidate = 15;
 
 export const metadata = {
-  title: "Cabins page",
+  title: "Cabins",
 };
 
-export default function Cabins() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -20,8 +27,14 @@ export default function Cabins() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
